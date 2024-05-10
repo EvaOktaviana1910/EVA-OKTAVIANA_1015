56b37b0fb112c8bf7332cc1bf90163ea69b78a10
Eva Oktaviana <146150586+EvaOktaviana1910@users.noreply.github.com>
2024-05-10 21:05:44 +0700

    Create Routes.php

---
+++ Controller/Routes.php
@@ -0,0 +1,18 @@
+<?php
+// routes.php
+require 'env.php';
+require 'function.php';
+require_once 'Models/models.php';
+
+
+Router::url('index', 'get', 'index.php');
+Router::url('register', 'get', 'register.php');
+Router::url('login', 'get', 'login.php');
+Router::url('insertcontactapps', 'post', 'insertcontactapps.php');
+Router::url('updatecontactapps', 'get', 'edit_contactapps.php');
+Router::url('deletecontactapps', 'get', 'delete-contactapps.php');
+
+
+Router::url('/', 'get', function () {
+    header('Location: login');
+});
